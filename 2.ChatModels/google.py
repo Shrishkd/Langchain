c4eba33164a24b80f6314184ee949725ead9734a
Shrishkd <shrishdas444@gmail.com>
2026-06-14 16:52:31 +0530

    chatmodels

--- 2.ChatModels/google.py
+++ 2.ChatModels/google.py
@@ -1,1 +1,10 @@
-from langchain_
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-3.5-flash', temperature=0)
+
+result = model.invoke('Which is the sixth largest city of Bihar')
+
+print(result.content)
